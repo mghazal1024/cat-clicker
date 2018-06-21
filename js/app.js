da508f80c32d857d5136754346315b0c955d6af0
Mohamad Ghazal <mghazal1024@gmail.com>
2018-06-21 22:12:12 +0200

    Created the Model View Octopus with Vanilla JS

--- js/app.js
+++ js/app.js
@@ -1,51 +1,52 @@
-catNames = ["Nounou", "Nani", "Mike", "Bella", "Luna"];
-
-function Cat(name, count) {
-  this.image = `images/${name}.png`;
-  this.name = name;
-  this.alt = name;
-  this.count = count;
+let model = {
+  cats : [
+    {
+      name: "Nounou",
+      count: 0,
+    },
+    {
+      name: "Nani",
+      count: 0,
+    },
+    {
+      name: "Bella",
+      count: 0,
+    },
+    {
+      name: "Luna",
+      count: 0,
+    },
+    {
+      name: "Leo",
+      count: 0,
+    }
+  ]
 }
 
-for (i = 0; i < catNames.length; i++) {
-  window[`${catNames[i]}`] = new Cat(`${catNames[i]}`, 0);
-}
-
-const catList = document.querySelector('.catList ul');
-const selectedCatContainer = document.querySelector('.selectedCat');
-const catImage = document.querySelector('.catImage');
-let catCounting = document.querySelector('.catCounting');
-let clickCount = 0;
-let catListContent;
-let catListLink;
-
-
-for(let cat of catNames) {
-  catListContent = document.createElement('li');
-  catListLink = document.createElement('a');
-  catListLink.className = 'catLink';
-  catListLink.setAttribute('href', '#');
-  catListLink.textContent = `${cat}`;
-  catListContent.appendChild(catListLink);
-  catList.appendChild(catListContent);
-
-  catListLink.addEventListener('click', function() {
-    console.log(`This ${cat} was clicked`);
-    catImage.setAttribute('src', `images/${cat}.png`);
-    catImage.setAttribute('alt', `${cat}`);
-  });
-}
-
-/*
-catImage.addEventListener('click', function() {
-  clickCount += 1;
-  catCounting.textContent = clickCount;
-})
-*/
-
-catImage.addEventListener('click', (function(clickCountCopy) {
-  return function() {
-    clickCountCopy += 1;
-    catCounting.textContent = clickCountCopy;
+let octopus = {
+  getCats: function() {
+    return model.cats;
   }
-})(clickCount));
+}
+
+let view = {
+  init: function() {
+
+  },
+
+  renderList: function() {
+    const catsContainer = document.querySelector('.catList ul')
+    //let catLi = document.createElement('li');
+    octopus.getCats().forEach(function(cat) {
+      console.log(cat.name);
+      catsContainer.innerHTML += `<li><a class="catLink" href="#">${cat.name}</a></li>`
+      console.log(catsContainer);
+    })
+  },
+
+  renderCat: function() {
+
+  }
+}
+
+view.renderList();
